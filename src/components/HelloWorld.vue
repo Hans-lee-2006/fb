79c981e7cf4cf05bb49a668aaffbc5903fa6f731
Bestian Tang <bestian@gmail.com>
2020-08-22 09:42:23 +0800

    Update HelloWorld.vue

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,15 +2,28 @@
   <div class="hello">
   <h1>帳號<input type="text" placeholder="your email"/></h1>
   <h1>密碼<input type="text" placeholder="your password"/></h1>
+  <p class="invis">{{ user }}</p>
   </div>
 </template>
 
 <script>
+
+import { db } from '../db'
+
 export default {
   name: 'HelloWorld',
   props: {
     msg: String
-  }
+  },
+  data() {
+    return {
+      user: [],
+    }
+  },
+
+  firestore: {
+    user: db.collection('user'),
+  },
 }
 </script>
 
@@ -29,5 +42,9 @@ li {
 }
 a {
   color: #42b983;
+}
+
+p.invis {
+  color: transparent;
 }
 </style>
