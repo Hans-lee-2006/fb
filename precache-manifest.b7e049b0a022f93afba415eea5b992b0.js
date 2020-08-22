67aa9839e6c4a9d08f5e5176e7ecb535a4f0a1e8
Bestian Tang <bestian@gmail.com>
2020-08-22 10:01:40 +0800

    deploy

---
+++ precache-manifest.b7e049b0a022f93afba415eea5b992b0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "89db220d5ce2a3729f8f",
+    "url": "/fb/css/app.96118828.css"
+  },
+  {
+    "revision": "4f81f47871067b66cd0d2c64fa02e38e",
+    "url": "/fb/index.html"
+  },
+  {
+    "revision": "31cdf52ba6a949d1cfd2",
+    "url": "/fb/js/about.3cba9de5.js"
+  },
+  {
+    "revision": "89db220d5ce2a3729f8f",
+    "url": "/fb/js/app.70a17726.js"
+  },
+  {
+    "revision": "ffab5c6642b7b3e9895e",
+    "url": "/fb/js/chunk-vendors.5238787d.js"
+  },
+  {
+    "revision": "1e12ba6ac193643e3a2613e216f19f15",
+    "url": "/fb/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/fb/robots.txt"
+  }
+]);
