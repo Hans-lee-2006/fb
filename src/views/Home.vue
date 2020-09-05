c8b25d6feac2fb4cc0690351008b004e26443e7c
Hans-lee-2006 <hanslee2006@gmail.com>
2020-09-05 09:28:11 +0800

    試圖使圖片可以拖動

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="https://static.xx.fbcdn.net/rsrc.php/y8/r/dF5SId3UHWd.svg">
+    <img class="draggable" alt="Vue logo" src="https://static.xx.fbcdn.net/rsrc.php/y8/r/dF5SId3UHWd.svg">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
@@ -8,12 +8,17 @@
 <script>
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
+import $ from 'jquery' 
+import 'jquery-ui-dist/jquery-ui'  
+window.jQuery = $ 
+require('jquery-ui-touch-punch')
 
 export default {
   name: 'Home',
   components: {
     HelloWorld
-  }
+  },
+  mounted () {     $('.drag').draggable();   } 
 }
 </script>
 
